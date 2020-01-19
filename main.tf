a31c7d47146dee1ac6844746eee65168c3101abe
kamakell <55107981+kamakell@users.noreply.github.com>
2020-01-19 12:13:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,9 @@ resource "google_compute_instance" "default" {
   name         = "vm-arjun"
   machine_type = "f1-micro"
   zone         = "eu-west2"
+  network_interface {
+    network = "dev"
+  }
   boot_disk {
     initialize_params {
       image     =  "centos-7-v20180129"     
