676272c01ffdf91cfd490a1a0dc6caa635d0fcbb
kamakell <55107981+kamakell@users.noreply.github.com>
2020-01-19 11:57:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ name         = "${var.vm_instance}-shy"
 
  resource "google_compute_network" "vpc_network" {
  name = "dev-vpc"
-}
   network_interface {
     # A default network is created for all GCP projects
     network       = "${google_compute_network.vpc_network.self_link}"
